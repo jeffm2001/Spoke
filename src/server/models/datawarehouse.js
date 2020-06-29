db8da2182c5cd276cf4213bd8c6772eefa02dce8
Jeff Mann <jmann@aflcio.org>
2020-06-29 11:06:51 -0400

    Add option for SOCKS5 proxy

--- src/server/models/datawarehouse.js
+++ src/server/models/datawarehouse.js
@@ -9,6 +9,7 @@ const {
   WAREHOUSE_DB_USER,
   // DB_SCHEMA,
   WAREHOUSE_USE_SSL = "false",
+  WAREHOUSE_IP_BURGER
 } = process.env;
 
 const useSSL = WAREHOUSE_USE_SSL === "1"
@@ -28,6 +29,33 @@ if (WAREHOUSE_DB_TYPE) {
       ssl: useSSL
     }
   };
+}
+
+if (WAREHOUSE_IP_BURGER) {
+  const SocksConnection = require('socksjs');
+  const proxyUrl = process.env.IPB_SOCKS5;
+  const proxyValues = proxyUrl.split(new RegExp('[/(:\\/@)/]+'));
+  const warehouse = {
+    host: WAREHOUSE_DB_HOST,
+    port: WAREHOUSE_DB_PORT
+  }
+  const proxyConnection = new SocksConnection(warehouse, {
+    user: proxyValues[0],
+    pass: proxyValues[1],
+    host: proxyValues[2],
+    port: proxyValues[3],
+  });
+
+  config = {
+    client: WAREHOUSE_DB_TYPE,
+    connection: {
+      stream: proxyConnection,
+      database: WAREHOUSE_DB_NAME,
+      password: WAREHOUSE_DB_PASSWORD,
+      user: WAREHOUSE_DB_USER,
+      ssl: useSSL
+    }
+  }
 }
 
 export default WAREHOUSE_DB_TYPE ? () => knex(config) : null;
